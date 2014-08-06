54954ab142aa092a7ba771e451eba2c40ca83830
Surendra Shrestha <shopuz@gmail.com>
2014-08-06 14:50:58 +1000

    another syntax added

--- prerequisite_parser.py
+++ prerequisite_parser.py
@@ -20,13 +20,26 @@ Some Grammars:
 word = pp.Word(pp.alphas)
 num = pp.Word(pp.nums)
 wn = word + num
-nw = num + word 
+nw = num + word
+alphanum_brackets = pp.Word(pp.alphanums + "(" + ")" )
+nw_cp = num + "cp" 
 range_expr = wn + "-" + wn
-pre_req_syntax_1 = nw + ( word + range_expr )
+# 3cp from MATH132-MATH136
+pre_req_syntax_1 = nw_cp + ( word + range_expr )
 
+# 18cp including (COMP115 or COMP155)
+pre_req_syntax_2 = nw_cp + "including" + alphanum_brackets
 
 # prereq unit range MATH132-MATH136 = ['MATH132', 'MATH133', 'MATH134', 'MATH135', 'MATH136']
 unit_range = []
+cp_rule = {"undergraduate": 3, "postgraduate": 4}
+syntax1_flag = False
+syntax2_flag = False
+
+student_units = [ 'COMP115', 'COMP165', 'MATH132', 'DMTH137', 'MATH134', 'MATH135', 'MATH136']
+#student_units = ['COMP125', 'MATH136']
+#main_pre_req = pre_req =  "(COMP125(P) or COMP165(P)) and (3cp(P) from MATH132-MATH136 or DMTH137)"
+main_pre_req = pre_req = "18cp including (COMP115 or COMP155)"
 
 # Calculate Total Credit Points obtained by the student
 # To check "3cp from MATH132-MATH136"
@@ -38,6 +51,11 @@ def cp_calculator(student_units, unit_list):
 			cp = cp + 3
 	return cp
 
+# Calculate the total credit points obtained by the student based on his completed units
+def total_cp(student_units, level="undergraduate"):
+	total_cp_gained = len(student_units) * cp_rule[level]
+	return total_cp_gained
+
 
 def prepare_unit_range(unit_name, starting_unit, ending_unit):
 	i = starting_unit
@@ -47,24 +65,43 @@ def prepare_unit_range(unit_name, starting_unit, ending_unit):
 
 
 def parse_pre_req_syntax_1(text):
+	global pre_req
 	# tokens = (['3', 'cp', 'from', 'MATH', '132', '-', 'MATH', '136'], {})
-	tokens = list(pre_req_syntax_1.parseString(text))
-	cp_required = int(tokens[0])
+	if syntax1_flag:
+		tokens = list(pre_req_syntax_1.parseString(text))
+		# if the prerequisite contains a range
+	
+		range_index = tokens.index('-')
+		unit_name = tokens[range_index + 1]
+		starting_unit = int(tokens[range_index - 1])
+		ending_unit = int(tokens[range_index + 2])
+		prepare_unit_range(unit_name, starting_unit, ending_unit)
+		cp_required = int(tokens[0])
 
-	unit_name = tokens[tokens.index('-') + 1]
-	starting_unit = int(tokens[tokens.index('-') - 1])
-	ending_unit = int(tokens[tokens.index('-') + 2])
-	prepare_unit_range(unit_name, starting_unit, ending_unit)
-	return cp_required
+		if cp_calculator(student_units, unit_range) == cp_required:
+			complex_statement_result = "True"
+			pre_req = pre_req.replace(complex_statement, complex_statement_result)
+		else:
+			pre_req = pre_req.replace(complex_statement, "False")
+	
+	elif syntax2_flag:
+		tokens = list(pre_req_syntax_2.parseString(text))
+		total_cp_gained = total_cp(student_units)
+		cp_required = int(tokens[0])
+		pre_req = pre_req.replace("including", "and")
 
+		if total_cp_gained >= cp_required:
+			pre_req = pre_req.replace(str(cp_required) + "cp", "True")
+		else:
+			pre_req = pre_req.replace(str(cp_required) + "cp", "False")
 
 
 if __name__ == '__main__':
-	student_units = [ 'DMTH137', 'COMP165']
+	
 
 	negative_words = [ 'or', 'and', 'including', 'from']
 
-	pre_req =  "(COMP125(P) or COMP165(P)) and (3cp(P) from MATH132-MATH136 or DMTH137)"
+	
 	pre_req = pre_req.replace('(P)', '')
 
 	# pre_req = "(COMP125 or COMP165) and (3cp from MATH132-MATH136 or DMTH137)"
@@ -76,19 +113,23 @@ if __name__ == '__main__':
 		for t,s,e in pre_req_syntax_1.scanString(req):
 			if t:
 				complex_statements.append(req)
+				syntax1_flag = True
 
+		for t,s,e in pre_req_syntax_2.scanString(req):
+			if t:
+				# for structure 18cp including COMP115
+				# need to convert 18cp into True or False as well
+				complex_statements.append(req)
+				syntax2_flag = True
 
+	#print "complex statements: ", complex_statements
 	for complex_statement in complex_statements:
-		cp_required = parse_pre_req_syntax_1(complex_statement)
-		if cp_calculator(student_units, unit_range) == cp_required:
-			complex_statement_result = "True"
-			pre_req = pre_req.replace(complex_statement, complex_statement_result)
-		else:
-			pre_req = pre_req.replace(complex_statement, "False")
+		parse_pre_req_syntax_1(complex_statement)
 
-
+	pre_req_unit_list = re.split(' and | or | including ', cleaned_pre_req)
 	# filtered_list = ['COMP125', 'COMP165', 'MATH132-MATH136', 'DMTH137']
-	filtered_list = [ x for x in pre_req_unit_list if x not in negative_words ]
+	filtered_list = [ x for x in pre_req_unit_list if ((x not in negative_words) and ('cp' not in x)) ]
+	#print 'pre_req_unit_list: ', pre_req_unit_list
 	#print 'filtered list: ', filtered_list
 
 	# Set all the units taken by student to True
@@ -104,13 +145,16 @@ if __name__ == '__main__':
 
 	#unit_list = [ 'MATH132', 'MATH133', 'MATH134', 'MATH135', 'MATH136' ]
 	#complex_statement = "3cp from MATH132-MATH136"
-
-	#print "pre_req: " , pre_req
+	print "student_units: ", student_units
+	print "main prerequisite statement: ", main_pre_req
+	print "pre_req: " , pre_req
+	print "------------------------"
 	if (eval(pre_req) == True):
-		print 'Yes'
+		print 'Result: Yes'
 	else:
-		print 'No'
+		print 'Result: No'
 
+	print "------------------------"
 
 
 # Replace 3cp from MATH132-MATH136 with true or false in the statement
