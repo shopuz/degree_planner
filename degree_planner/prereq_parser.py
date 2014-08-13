dde5716f8a0a890c8e23576acf4a932628c13809
Surendra Shrestha <shopuz@gmail.com>
2014-08-13 11:53:13 +1000

    parsing A or B or C and D is added

--- degree_planner/prereq_parser.py
+++ degree_planner/prereq_parser.py
@@ -4,26 +4,51 @@ class Prereq_Parser():
 	def __init__(self):
 		self.atomic = Word(alphanums)
 		self.expr = operatorPrecedence(self.atomic, [
-									("AND", 2, opAssoc.LEFT, ),
-									("OR", 2, opAssoc.LEFT, ),
+									("and", 2, opAssoc.LEFT, self.expandChainedExpr),
+									("or", 2, opAssoc.LEFT, self.expandChainedExpr ),
 								])
 		self.parsed_list = None
 		
 	def parse_string(self, prereq=None):
-		""" Pyparsing module to tokenize the prerequisite with AND and OR logical operators
+		""" Pyparsing module to tokenize the prerequisite with 'and' and 'or' logical operators
 			Parses the structure into a binary tree representation
 		"""
-		result =  self.expr.parseString(prereq, parseAll=True).asList()
-		result = self.arrange_operator(result[0])
-		return result
+		result =  self.expr.parseString(prereq).asList()
+		#result = self.arrange_operator(result[0])
+		return result[0]
+
+	def expandChainedExpr(self, tokens):
+		"""
+			Function to order the sequence of operands and operators based on their precedence.
+			Initial Prerequisite = 'COMP115 and (COMP111 or INFO111 or MAS111)'
+			Input: tokens = ['COMP115', 'and', 'COMP111', 'or', 'INFO111', 'or', 'MAS111']
+			Output: ret = ['COMP115', 'and', [['COMP111' , 'or', 'INFO111'], 'or', 'MAS111']]
+		"""
+		tokeniter = iter(tokens[0])
+		firstexpr = next(tokeniter)
+		# zips = [('or', 'COMP125'), ('and', 'MATH132')]
+		zips = zip(tokeniter,tokeniter)
+
+		ret = ParseResults([firstexpr])
+		zip_length = len(zips)
+		# ret = [['COMP124', 'or', 'COMP125']]
+		ret = ret + ParseResults(list(zips[0]))	
+		
+		# Loop through the zips if there are more than 1 items in zips
+		i = 1
+		while i < zip_length:
+			ret = ParseResults([ret]) + ParseResults(list(zips[i]))
+			i = i + 1
+		
+		return [ret]
 
 	def arrange_operator(self, parsed_list):
 		"""
 		Input: 	Parsed List from pyparsing
-			 	[['COMP115', 'OR', 'COMP125'], 'AND', 'MATH132']
+				[['COMP115', 'or', 'COMP125'], 'and', 'MATH132']
 		
 		Output: Binary Tree representation of form [operator, unit, unit]
-				['AND', ['OR', 'COMP115', 'COMP125'], 'MATH132']
+				['and', ['or', 'COMP115', 'COMP125'], 'MATH132']
 
 		"""
 		if len(parsed_list) == 3:
@@ -39,7 +64,10 @@ class Prereq_Parser():
 
 
 	def evaluate_prerequisite(self, pre_req_tree, student_units ):
-		""" ['OR', 'COMP115', 'COMP125'] """
+		""" 
+			['COMP125', 'or', 'COMP165'] 
+			['COMP225', 'or', 'COMP229', 'or', 'COMP125']
+		"""
 		if isinstance(pre_req_tree, str):
 			if pre_req_tree in student_units:
 				return True
@@ -47,18 +75,10 @@ class Prereq_Parser():
 				return False
 		elif isinstance(pre_req_tree, list):
 			# do recursive call
-			if pre_req_tree[0] == 'OR':
-				temp = str(self.evaluate_prerequisite(pre_req_tree[1], student_units) or self.evaluate_prerequisite(pre_req_tree[2], student_units))
+			if pre_req_tree[1] == 'or':
+				temp = str(self.evaluate_prerequisite(pre_req_tree[0], student_units) or self.evaluate_prerequisite(pre_req_tree[2], student_units))
+				return eval(temp)
+			elif pre_req_tree[1] == 'and':
+				temp = str(self.evaluate_prerequisite(pre_req_tree[0], student_units) and self.evaluate_prerequisite(pre_req_tree[2], student_units))
 				print temp
 				return eval(temp)
-			elif pre_req_tree[0] == 'AND':
-				temp = str(self.evaluate_prerequisite(pre_req_tree[1], student_units) and self.evaluate_prerequisite(pre_req_tree[2], student_units))
-				print temp
-				return eval(temp)
-		
-
-		
-
-if __name__ == "__main__":
-	unittest.main(verbosity=2)
-	
--- degree_planner/tests/test_parser.py
+++ degree_planner/tests/test_parser.py
@@ -7,29 +7,32 @@ class ParseTestCase(unittest.TestCase):
 	def test_simple_logical(self):
 		pp = Prereq_Parser()
 		
-		result = pp.parse_string("COMP115 AND COMP125")
-		expected_result = ['AND', 'COMP115', 'COMP125']
+		result = pp.parse_string('COMP115')
+		expected_result = 'COMP115'
 		self.assertEqual(result, expected_result)
-		
-		result = pp.parse_string("(COMP115 OR COMP125) AND MATH132")
-		expected_result = ['AND', ['OR', 'COMP115', 'COMP125'], 'MATH132']
+		result = pp.parse_string('COMP125 or COMP165')
+		expected_result = ['COMP125','or', 'COMP165']
 		self.assertEqual(result, expected_result)
 
-		result = pp.parse_string("(COMP165 AND ((COMP115 OR COMP125) OR (MATH132 OR MATH136)))")
-		expected_result = ['AND', 'COMP165', ['OR', ['OR', 'COMP115', 'COMP125'], ['OR', 'MATH132', 'MATH136' ]]]
+		result = pp.parse_string('COMP225 or COMP229 or COMP125')
+		expected_result = [['COMP225', 'or', 'COMP229'], 'or', 'COMP125']
+		self.assertEqual(result, expected_result)
+
+		result = pp.parse_string('COMP115 and (COMP111 or INFO111 or MAS111)')
+		expected_result = ['COMP115', 'and', [['COMP111' , 'or', 'INFO111'], 'or', 'MAS111']]
 		self.assertEqual(result, expected_result)
 
 	def test_evaluate_prereq(self):
 		pp = Prereq_Parser()
-		pre_req_tree = ['OR', 'COMP115', 'COMP125']
+		pre_req_tree = ['COMP115', 'or', 'COMP125']
 		student_units = ['COMP125', 'COMP115', 'COMP165']
 		self.assertTrue(pp.evaluate_prerequisite(pre_req_tree, student_units))
 
-		pre_req_tree = ['AND', ['OR', 'COMP115', 'COMP125'], 'MATH132']
-		self.assertFalse(pp.evaluate_prerequisite(pre_req_tree, student_units))
+		pre_req_tree = [['COMP225', 'or', 'COMP229'], 'or', 'COMP125']
+		self.assertTrue(pp.evaluate_prerequisite(pre_req_tree, student_units))
 
-		student_units = ['COMP115', 'MATH132']
-		self.assertTrue(pp.evaluate_prerequisite(pre_req_tree, student_units))		
+		student_units =['COMP115', 'and', [['COMP111' , 'or', 'INFO111'], 'or', 'MAS111']]
+		self.assertFalse(pp.evaluate_prerequisite(pre_req_tree, student_units))		
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	unittest.main(verbosity=2)
